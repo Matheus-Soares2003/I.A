ad23e1ac636f1b74219f16f759b85fae9e0da421
Matheus-Soares2003 <matheusstcabral@gmail.com>
2022-05-04 12:13:12 -0300

    Criacao de algoritimo tabuSearch basico

---
+++ tabuSearch/Loja_711.R
@@ -0,0 +1,11 @@
+library(tabuSearch)
+
+z = tabuSearch(size = 12, iters = 1000, objFunc = compras,
+               listSize = 9, nRestarts = 10, repeatAll = 1,
+               verbose = T)
+
+plot(z)
+summary(z)
+
+z$configkeep[299,]
+summary(z, verbose = TRUE)
